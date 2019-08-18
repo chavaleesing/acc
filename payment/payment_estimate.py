f651918ef7669b28e831528ed2652f744a8acc70
chavaleesing <chavalee.sing@gmail.com>
2019-08-18 15:10:30 +0700

    [acc] update 1

--- payment/payment_estimate.py
+++ payment/payment_estimate.py
@@ -2,16 +2,16 @@ from datetime import datetime
 import random
 import xlsxwriter
 
-
+# venv\Scripts\activate
 # month_payment_estimate = input("Enter your month payment estimation: ")
 # print(f"Payment Estimation = {month_payment_estimate} Baht.") 
 # month_payment_estimate = int(month_payment_estimate)
-month_payment_estimate = 2972400
+month_payment_estimate = 2405275
 
 # amount_slip_list = input("Enter your amount_slip_list: ")
 # amount_slip_list = amount_slip_list.split(',')
 # print(f"Amount of slip list = {amount_slip_list} slips.")
-amount_slip_list=[31,6,30,36,22,38,8,15,0,7,11,31,30,11,17,6,8,16,41,34,5,13,17,19,14,27,36,7,9,10,46]
+amount_slip_list=[34,41,4,7,13,21,22,36,37,10,11,12,14,20,37,38,6,7,20,19,12,34,34,9,9,17,14,8,36,42]
 
 def get_random_per_day(month_payment_estimate, amount_slip_list):
     rand_list = list()
@@ -21,8 +21,8 @@ def get_random_per_day(month_payment_estimate, amount_slip_list):
         if index == len(amount_slip_list)-1:
             rand = month_payment_estimate - total
         else:
-            rand_from  = item*4900
-            rand_to = item*5100
+            rand_from  = item*2700
+            rand_to = item*6200
             rand = random.randint(rand_from,rand_to)
             temp = rand%5
             rand = rand - temp
@@ -58,9 +58,15 @@ def get_random_per_bill(rand_list, amount_slip_list):
                 r = r=random.randint(x-500,x+500)
             elif x == bill_amount-4:
                 x = int((total_payment-total)/4)
-                r = r=random.randint(x-100,x+100)
+                r = r=random.randint(x-500,x+500)
             elif x == bill_amount-8:
                 x = int((total_payment-total)/8)
+                r = r=random.randint(x-100,x+100)
+            elif x == bill_amount-10:
+                x = int((total_payment-total)/10)
+                r = r=random.randint(x-100,x+100)
+            elif x == bill_amount-13:
+                x = int((total_payment-total)/13)
                 r = r=random.randint(x-100,x+100)
             temp = r%5
             r = r-temp
@@ -73,18 +79,18 @@ def get_random_per_bill(rand_list, amount_slip_list):
 
 def gen_excel(estimate_result):
     print(estimate_result)
-    workbook = xlsxwriter.Workbook('Expenses01.xlsx')
+    workbook = xlsxwriter.Workbook('Expenses02.xlsx')
     worksheet = workbook.add_worksheet()
 
     row = 0
     col = 0
     order=estimate_result.keys()
     for key in order:
-        row += 1
-        worksheet.write(row, col,     key)
+        col += 1
+        worksheet.write(col, row,     key)
         i =1
         for item in estimate_result[key]:
-            worksheet.write(row, col + i, item)
+            worksheet.write(col, row + i, item)
             i += 1
     workbook.close()
     return None
