79f3f5aac2ab680d76f578bb462444a79a46e8be
Chavalee J <chavaleej@Chavalees-MacBook-Pro.local>
2023-07-03 21:59:28 +0700

    update

--- evil/expense_generator.py
+++ evil/expense_generator.py
@@ -3,10 +3,10 @@ import xlsxwriter
 
 def generate_random_integers(_sum, n):  
     mean = int(_sum / n)
-    variance = int(0.3 * mean)
+    variance = int(0.25 * mean)
 
     min_v = mean - variance
-    max_v = mean + int(0.15 * mean)
+    max_v = mean + int(0.125 * mean)
     array = [min_v] * n
     # print(min_v)
     # print(max_v)
@@ -14,7 +14,7 @@ def generate_random_integers(_sum, n):
     diff = _sum - min_v * n
 
     for i in [0,1,2,-1,-2,-3]:
-        delta = random.randint(int(variance*0.68), int(variance*0.7))
+        delta = random.randint(int(variance*0.6), int(variance*0.69))
         diff -= delta
         array[i] = array[i] + delta
 
@@ -23,11 +23,11 @@ def generate_random_integers(_sum, n):
             break
         if array[a] >= max_v:
             continue
-        delta = random.randint(int(variance*0.4), int(variance*0.8))
+        delta = random.randint(int(variance*0.45), int(variance*0.7))
         array[a] += delta
         diff -= delta
         if array[a] >= max_v:
-            delta2 = random.randint(int(variance*0.3), int(variance*0.7))
+            delta2 = random.randint(int(variance*0.32), int(variance*0.6))
             array[a] -= delta2
             diff += delta
 
@@ -48,7 +48,7 @@ def generate_random_integers(_sum, n):
 
 
 def gen_excel(array, array2):
-    workbook = xlsxwriter.Workbook('พฤศจิกายน.xlsx')
+    workbook = xlsxwriter.Workbook('เมย.xlsx')
     worksheet = workbook.add_worksheet()
 
     col = 0
@@ -63,6 +63,6 @@ def gen_excel(array, array2):
     workbook.close()
     return None
 
-arr1 = generate_random_integers(35196825,30) ## ยอดขายที่ต้องเสียภาษี
-arr2 = generate_random_integers(2057440,30) ## ยอดขายที่ได้รับยกเว้น
+arr1 = generate_random_integers(28391540,30) ## ยอดขายที่ต้องเสียภาษี
+arr2 = generate_random_integers(1842530,30) ## ยอดขายที่ได้รับยกเว้น
 gen_excel(arr1, arr2)
